de08ae6c1cbee3b9589ec86dafbd736acc2a56df
brendanajeraflores <36040244+brendanajeraflores@users.noreply.github.com>
2018-07-27 12:27:22 -0500

    Add files via upload

--- insert.js
+++ insert.js
@@ -10,25 +10,25 @@ db.etiquetas.insert({ field: 'recid', caption: 'ID', size: '50px', sortable: tru
 ])
 
 //Sentencia para insertar documentos en la colección records
-db.records.insert({ recid: 1, nombre: 'Jane', apellido: 'Pérez', email: 'jdoe@gmail.com', monto: 2500, fechaini: ISODate("2018-02-02T21:27:35.783Z")})
+db.records.insert({ recid: 1, nombre: 'Jane', apellido: 'Pérez', email: 'jdoe@gmail.com', monto: 2500, fechaini: "2/2/2018")})
 
-db.getCollection('records').insert([{ recid: 2, nombre: 'Stuart', apellido: 'Motzart', email: 'jdoe@gmail.com', monto: 1004, fechaini: ISODate("2018-02-27T21:27:35.783Z") },
-            { recid: 3, nombre: 'Jin', apellido: 'Franson', email: 'peter@gmail.com', monto: 473, fechaini: ISODate("2018-01-01T21:27:35.783Z") },
-            { recid: 4, nombre: 'Susan', apellido: 'Ottie', email: 'jim@gmail.com', monto: 304.12, fechaini: ISODate("2018-11-13T21:27:35.783Z") },
-            { recid: 5, nombre: 'Kelly', apellido: 'Vázquez', email: 'peter@gmail.com', monto: 9300.23, fechaini: ISODate("2018-10-19T21:27:35.783Z") },
-            { recid: 6, nombre: 'Francis', apellido: 'Gatos', email: 'jdoe@gmail.com', monto: 3400, fechaini: ISODate("2018-05-12T21:27:35.783Z") },
-            { recid: 7, nombre: 'Mark', apellido: 'Welldo', email: 'jim@gmail.com', monto: 2030, fechaini: ISODate("2018-06-04T21:27:35.783Z") },
-            { recid: 8, nombre: 'Thomas', apellido: 'Bahh', email: 'jdoe@gmail.com', monto: 13004, fechaini: ISODate("2018-07-05T21:27:35.783Z") },
-            { recid: 9, nombre: 'Sergei', apellido: 'Rachmaninov', email: 'jdoe@gmail.com', monto: 4043, fechaini: ISODate("2018-12-16T21:27:35.783Z")},
-            { recid: 20, nombre: 'Jill', apellido: 'Doe', email: 'jdoe@gmail.com', monto: 474, fechaini: ISODate("2018-06-30T21:27:35.783Z") },
-            { recid: 21, nombre: 'Frank', apellido: 'Motzart', email: 'jdoe@gmail.com', monto: 1704.25, fechaini: ISODate("2018-02-02T21:27:35.783Z") },
-            { recid: 22, nombre: 'Peter', apellido: 'Franson', email: 'jdoe@gmail.com', monto: 7065, fechaini:ISODate("2018-02-27T21:27:35.783Z") },
-            { recid: 23, nombre: 'Andrew', apellido: 'Ottie', email: 'jdoe@gmail.com', fechaini: ISODate("2018-01-01T21:27:35.783Z") },
-            { recid: 24, nombre: 'Manny', apellido: 'Silver', email: 'jdoe@gmail.com', fechaini: ISODate("2018-02-02T21:27:35.783Z") },
-            { recid: 25, nombre: 'Ben', apellido: 'Gatos', email: 'peter@gmail.com', fechaini: ISODate("2018-02-02T21:27:35.783Z") },
-            { recid: 26, nombre: 'Doer', apellido: 'Welldo', email: 'jdoe@gmail.com', fechaini: ISODate("2018-02-27T21:27:35.783Z") },
-            { recid: 27, nombre: 'Shashi', apellido: 'Bahh', email: 'jim@gmail.com', fechaini: ISODate("2018-02-27T21:27:35.783Z") },
-            { recid: 28, nombre: 'Av', apellido: 'Rachmaninov', email: 'jim@gmail.com', fechaini: ISODate("2018-02-27T21:27:35.783Z") }
+db.getCollection('records').insert([{ recid: 2, nombre: 'Stuart', apellido: 'Motzart', email: 'jdoe@gmail.com', monto: 1004, fechaini: "2/27/2018" },
+{ recid: 3, nombre: 'Jin', apellido: 'Franson', email: 'peter@gmail.com', monto: 473, fechaini: "1/1/2018" },
+{ recid: 4, nombre: 'Susan', apellido: 'Ottie', email: 'jim@gmail.com', monto: 304.12, fechaini: "11/13/2018" },
+{ recid: 5, nombre: 'Kelly', apellido: 'Vázquez', email: 'peter@gmail.com', monto: 9300.23, fechaini: "10/19/2018" },
+{ recid: 6, nombre: 'Francis', apellido: 'Gatos', email: 'jdoe@gmail.com', monto: 3400, fechaini: "5/12/2018" },
+{ recid: 7, nombre: 'Mark', apellido: 'Welldo', email: 'jim@gmail.com', monto: 2030, fechaini: "6/4/2018" },
+{ recid: 8, nombre: 'Thomas', apellido: 'Bahh', email: 'jdoe@gmail.com', monto: 13004, fechaini: "7/5/2018" },
+{ recid: 9, nombre: 'Sergei', apellido: 'Rachmaninov', email: 'jdoe@gmail.com', monto: 4043, fechaini: "12/16/18"},
+{ recid: 20, nombre: 'Jill', apellido: 'Doe', email: 'jdoe@gmail.com', monto: 474, fechaini: "6/30/2018" },
+{ recid: 21, nombre: 'Frank', apellido: 'Motzart', email: 'jdoe@gmail.com', monto: 1704.25, fechaini: "2/2/2018" },
+{ recid: 22, nombre: 'Peter', apellido: 'Franson', email: 'jdoe@gmail.com', monto: 7065, fechaini:"2/27/2018" },
+{ recid: 23, nombre: 'Andrew', apellido: 'Ottie', email: 'jdoe@gmail.com', fechaini: "1/1/2018" },
+{ recid: 24, nombre: 'Manny', apellido: 'Silver', email: 'jdoe@gmail.com', fechaini: "2/2/2018" },
+{ recid: 25, nombre: 'Ben', apellido: 'Gatos', email: 'peter@gmail.com', fechaini: "2/2/2018"},
+{ recid: 26, nombre: 'Doer', apellido: 'Welldo', email: 'jdoe@gmail.com', fechaini: "2/27/2018" },
+{ recid: 27, nombre: 'Shashi', apellido: 'Bahh', email: 'jim@gmail.com', fechaini: "2/27/2018" },
+{ recid: 28, nombre: 'Av', apellido: 'Rachmaninov', email: 'jim@gmail.com', fechaini: "2/27/2018" }
 ])
 
 //Sentencia para insertar documentos en la colección searches
